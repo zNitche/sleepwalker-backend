a336fc2d1adeacec7eb46396fd3cb8bb7a08413a
zNitche <53448802+zNitche@users.noreply.github.com>
2023-08-31 20:19:36 +0200

    custom user model

--- sleepwalker/apps/authenticate/admin.py
+++ sleepwalker/apps/authenticate/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from sleepwalker.apps.authenticate.forms import CustomUserCreationForm, CustomUserChangeForm
+from sleepwalker.apps.authenticate.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    list_display = ("username", "is_staff", "is_active")
+    list_filter = ("username", "is_staff", "is_active")
+
+    fieldsets = (
+        ("Details", {
+            "fields": ("username", "password", "date_joined")
+        }),
+        ("Permissions", {
+            "fields": ("is_staff", "is_active")
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username", "password1", "password2", "is_staff",
+                "is_active"
+            )}
+         ),
+    )
+
+    search_fields = ("username",)
+    ordering = ("username",)
+    readonly_fields = ("date_joined",)
+
+
+admin.site.register(User, CustomUserAdmin)
--- sleepwalker/apps/authenticate/models.py
+++ sleepwalker/apps/authenticate/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from datetime import datetime
+from sleepwalker.apps.authenticate.managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=25, unique=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=datetime.now)
+
+    private_storage_space = models.IntegerField(default=0, null=False)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
