81efde1c7525e9066f3a29164b77d4ae06d5c2df
zNitche <53448802+zNitche@users.noreply.github.com>
2023-09-07 19:15:43 +0200

    api view + serializer for environment sensors

---
+++ sleepwalker/apps/core/class_views/environment_sensors_view.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from sleepwalker.apps.core import models
+from sleepwalker.apps.core.serializers.environment_sensors_logs_serializer import EnvironmentSensorsLogsSerializer
+from sleepwalker.apps.authenticate.token_auth import TokenAuth
+
+
+class EnvironmentSensorsView(APIView):
+    authentication_classes = [TokenAuth]
+
+    def get(self, request, *args, **kwargs):
+        log_session = get_object_or_404(models.LogSession, uuid=self.kwargs["session_uuid"])
+        serializer = EnvironmentSensorsLogsSerializer(log_session.environment_sensors_logs, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        session_uuid = self.kwargs["session_uuid"]
+        serializer = EnvironmentSensorsLogsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(session_uuid=session_uuid)
+
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- sleepwalker/apps/core/serializers/environment_sensors_logs_serializer.py
+++ sleepwalker/apps/core/serializers/environment_sensors_logs_serializer.py
@@ -5,4 +5,17 @@ from sleepwalker.apps.core import models
 class EnvironmentSensorsLogsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.EnvironmentSensorsLog
-        exclude = ["id", "log_session"]
+        fields = ["uuid", "date", "temperature", "humidity"]
+        read_only_fields = ["uuid", "date"]
+
+    def create(self, validated_data):
+        log_session = models.LogSession.objects.filter(uuid=validated_data["session_uuid"]).first()
+        obj = models.EnvironmentSensorsLog(
+            temperature=validated_data["temperature"],
+            humidity=validated_data["humidity"],
+            log_session=log_session
+        )
+
+        obj.save()
+
+        return obj
