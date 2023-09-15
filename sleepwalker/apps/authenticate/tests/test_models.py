d677dadb67af4dd237414d9c0d8012aaed489b7e
zNitche <53448802+zNitche@users.noreply.github.com>
2023-09-15 23:13:18 +0200

    tests for authenticate app

---
+++ sleepwalker/apps/authenticate/tests/test_models.py
@@ -0,0 +1,45 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from sleepwalker.apps.authenticate import models
+import datetime
+
+
+class TestModels(APITestCase):
+    def setUp(self):
+        self.username = "user"
+        self.password = "1234"
+
+        get_user_model().objects.create_user(username=self.username, password=self.password)
+
+    def test_new_user_creation(self):
+        get_user_model().objects.create_user(username="test_user", password="1234")
+        user = get_user_model().objects.filter(username="test_user").first()
+
+        self.assertIsNot(user, None)
+
+    def test_existing_user(self):
+        user = get_user_model().objects.filter(username=self.username).first()
+
+        self.assertIsNot(user, None)
+
+    def test_auth_token_creation(self):
+        user = get_user_model().objects.filter(username=self.username).first()
+        token = models.AuthToken.objects.create(user=user)
+
+        self.assertIsNot(token, None)
+        self.assertTrue(token.is_valid())
+        self.assertFalse(token.is_expired())
+
+    def test_auth_token_blacklist(self):
+        user = get_user_model().objects.filter(username=self.username).first()
+        token = models.AuthToken.objects.create(user=user, blacklisted=True)
+
+        self.assertFalse(token.is_valid())
+
+    def test_auth_token_expiration(self):
+        user = get_user_model().objects.filter(username=self.username).first()
+        past_date = datetime.datetime.utcnow() - datetime.timedelta(1)
+        token = models.AuthToken.objects.create(user=user, expiration_date=past_date)
+
+        self.assertFalse(token.is_valid())
+        self.assertTrue(token.is_expired())
---
+++ sleepwalker/apps/authenticate/tests/test_views.py
@@ -0,0 +1,52 @@
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth import get_user_model
+from sleepwalker.apps.authenticate import models
+
+
+class TestViews(APITestCase):
+    def setUp(self):
+        self.username = "user"
+        self.password = "1234"
+
+        self.user = get_user_model().objects.create_user(username=self.username, password=self.password)
+        self.auth_token = models.AuthToken.objects.create(user=self.user)
+
+        self.client = APIClient()
+
+    def test_login(self):
+        response = self.client.post("/api/auth/login/", {
+            "username": self.username,
+            "password": self.password
+        })
+
+        self.assertEquals(response.status_code, 200)
+        self.assertIsNot(response.json().get("token"), None)
+
+    def test_logout_auth(self):
+        self.client.credentials(HTTP_X_AUTHORIZATION=self.auth_token.key)
+        response = self.client.post("/api/auth/logout/")
+
+        self.assertEquals(response.status_code, 200)
+
+        auth_token = models.AuthToken.objects.filter(key=self.auth_token.key).first()
+        self.assertFalse(auth_token.is_valid())
+
+    def test_logout_not_auth(self):
+        response = self.client.post("/api/auth/logout/")
+        self.assertEquals(response.status_code, 401)
+
+    def test_api_key_creation_auth(self):
+        self.client.credentials(HTTP_X_AUTHORIZATION=self.auth_token.key)
+
+        self.assertIs(self.user.api_key, None)
+        response = self.client.post("/api/auth/api-key/")
+
+        user = get_user_model().objects.filter(username=self.username).first()
+
+        self.assertEquals(response.status_code, 200)
+        self.assertIsNot(user.api_key, None)
+        self.assertIsNot(response.json().get("api_key"), None)
+
+    def test_api_key_creation_not_auth(self):
+        response = self.client.post("/api/auth/api-key/")
+        self.assertEquals(response.status_code, 401)
