594dec1bbdb38a0c4bceca873f460e6058fdd597
zNitche <53448802+zNitche@users.noreply.github.com>
2023-09-15 23:19:51 +0200

    tests for core app

---
+++ sleepwalker/apps/core/tests/test_views.py
@@ -0,0 +1,29 @@
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth import get_user_model
+from sleepwalker.apps.authenticate import models
+
+
+class TestViews(APITestCase):
+    def setUp(self):
+        self.username = "user"
+        self.password = "1234"
+
+        self.user = get_user_model().objects.create_user(username=self.username, password=self.password)
+        self.auth_token = models.AuthToken.objects.create(user=self.user)
+
+        self.client = APIClient()
+
+    def test_health_check(self):
+        response = self.client.get("/api/health-check/")
+
+        self.assertEquals(response.status_code, 200)
+
+    def test_auth_check_as_auth(self):
+        self.client.credentials(HTTP_X_AUTHORIZATION=self.auth_token.key)
+        response = self.client.get("/api/auth-check/")
+
+        self.assertEquals(response.status_code, 200)
+
+    def test_auth_check_as_not_auth(self):
+        response = self.client.get("/api/auth-check/")
+        self.assertEquals(response.status_code, 401)
