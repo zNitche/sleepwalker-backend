d1d60a0ddbe984e924811b541977e4d7ff0a3aa0
zNitche <53448802+zNitche@users.noreply.github.com>
2023-09-19 21:29:31 +0200

    added ApiKey auth for auth-check endpoint

--- sleepwalker/apps/core/views.py
+++ sleepwalker/apps/core/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, authentication_classes
 from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes
+from sleepwalker.apps.authenticate.auth_handlers.api_key_auth import ApiKeyAuth
 from sleepwalker.apps.authenticate.auth_handlers.token_auth import TokenAuth
 
 
@@ -10,6 +11,6 @@ def health_check(request):
 
 
 @api_view(["GET"])
-@authentication_classes([TokenAuth])
+@authentication_classes([ApiKeyAuth, TokenAuth])
 def auth_check(request):
     return Response(status.HTTP_200_OK)
