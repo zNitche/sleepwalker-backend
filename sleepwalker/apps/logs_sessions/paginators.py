34ad701ba3cdeb41a9c041c09822b98a9b91e189
zNitche <53448802+zNitche@users.noreply.github.com>
2023-11-11 18:28:10 +0100

    tweaked logs sessions pagination

--- sleepwalker/apps/logs_sessions/paginators.py
+++ sleepwalker/apps/logs_sessions/paginators.py
@@ -5,11 +5,12 @@ from rest_framework.response import Response
 class LogsSessionsPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = "items"
-    max_page_size = 100
+    max_page_size = 20
 
     def get_paginated_response(self, data):
         return Response({
             "pagination": {
+                "pages": self.page.paginator.num_pages,
                 "current": self.page.number,
                 "next": self.get_next_link(),
                 "previous": self.get_previous_link(),
